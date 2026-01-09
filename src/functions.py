6ade68ce5ecc71fc0002ae8935dc00a4977d2792
jackngzx <jackngzx@gmail.com>
2026-01-09 22:02:54 +0800

    Adding functions to split images and links

--- src/functions.py
+++ src/functions.py
@@ -1,11 +1,13 @@
 from textnode import TextNode, TextType
+import re
+
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
         if node.text_type != TextType.TEXT:
             new_nodes.append(node)
-        
+
         else:
             # split the text of node with delimiter. result will be a list
             sections = node.text.split(delimiter)
@@ -19,5 +21,42 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 else:
                     new_node = TextNode(sections[i], text_type)
                     new_nodes.append(new_node)
-        
+
     return new_nodes
+
+
+def extract_markdown_images(text):
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+
+def extract_markdown_links(text):
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+
+        else:
+            image_tuples = extract_markdown_images(node.text)
+            if image_tuples == []:
+                new_nodes.append(node)
+            current_text = node.text
+            for image in image_tuples:
+                image_alt = image[0]
+                image_url = image[1]
+                sections = current_text.split(f"![{image_alt}]({image_url})" , 1)
+                if sections[0] is not None:
+                    new_nodes.append(TextNode(sections[0], TextType.TEXT))
+                    new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_url))
+                    current_text = sections[1]
+                else:
+                    new_nodes.append(TextNode(current_text, TextType.TEXT))
+    return new_nodes
+
+            
+def split_nodes_link(old_nodes):
+
--- src/test_functions.py
+++ src/test_functions.py
@@ -1,6 +1,10 @@
 import unittest
 
-from functions import split_nodes_delimiter
+from functions import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
+)
 from textnode import TextNode, TextType
 
 
@@ -9,10 +13,10 @@ class TestSplitNodes(unittest.TestCase):
         node = TextNode("This is text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
         result = [
-                    TextNode("This is text with a ", TextType.TEXT),
-                    TextNode("code block", TextType.CODE),
-                    TextNode(" word", TextType.TEXT),
-                ]
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
         self.assertEqual(new_nodes, result)
 
     def test_bold(self):
@@ -21,10 +25,31 @@ class TestSplitNodes(unittest.TestCase):
         ]
         new_nodes = split_nodes_delimiter(old_nodes, "**", TextType.BOLD)
         result = [
-                    TextNode("This is text with a ", TextType.TEXT),
-                    TextNode("bold", TextType.BOLD),
-                    TextNode(" word", TextType.TEXT),
-                ]
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ]
         self.assertEqual(new_nodes, result)
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to youtube", "https://www.youtube.com/@bootdotdev"),
+            ],
+            matches,
+        )
+
+
 if __name__ == "__main__":
     unittest.main()
+
