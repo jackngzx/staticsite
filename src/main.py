d725811ebf75ac08176a632c2a6a83f8443013e4
jackngzx <jackngzx@gmail.com>
2026-01-12 15:35:28 +0800

    pushing site on Pages

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ from functions import copy_content, generate_pages_recursive
 
 def main():
     source = "content"
-    destination = "public"
+    destination = "docs"
     basepath = "/"
-    basepath = sys.argv[1]
+    if len(sys.argv) > 1:
+        basepath == sys.argv[1]
 
     if os.path.exists(destination):
         shutil.rmtree(destination)
-    copy_content("static", "public")
+    copy_content("static", destination)
     generate_pages_recursive(source, "template.html", destination, basepath)
     
 main()
